c811828a0771c1b5a5898223479002513db7a785
Cody Yantis <cyantis@gmail.com>
2019-03-31 21:43:45 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ Bundler.require
 module Concerns
   module Findable
     def find_by_name(name)
-      self.all.find {|s| s.name == name}
+      all.find {|s| s.name == name}
     end
 
     def find_or_create_by_name(name)
-      self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+      find_by_name(name) ? find_by_name(name) : create(name)
     end
 
     def destroy_all
