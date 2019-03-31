2638afc7f5dafb599257d84d34a68bddaa5938ef
Cody Yantis <cyantis@gmail.com>
2019-03-31 21:52:24 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,13 +3,9 @@ require_relative "../config/environment.rb"
 class Genre
   extend Concerns::Findable
   include Concerns::InstanceMethods
+  
   attr_accessor :name
   @@all = []
-
-  #def initialize(name)
-  #  @name = name
-  #  @songs = []
-  #end
 
   def self.all
     @@all
