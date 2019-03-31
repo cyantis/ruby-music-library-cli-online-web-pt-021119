b51320662acb95e7f4e374f7cbf91122b9831ca9
Cody Yantis <cyantis@gmail.com>
2019-03-31 21:53:11 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,13 +3,13 @@ require_relative "../config/environment.rb"
 class Genre
   extend Concerns::Findable
   include Concerns::InstanceMethods
-  
-  attr_accessor :name
-  @@all = []
 
-  def self.all
-    @@all
-  end
+  attr_accessor :name
+  #@@all = []
+#
+  #def self.all
+  #  @@all
+  #end
 
   def self.create(name)
     Genre.new(name).tap {|s| s.save}
