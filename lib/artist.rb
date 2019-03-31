b0ee2294f39a1815cf4e560718cf3f9520b385c0
Cody Yantis <cyantis@gmail.com>
2019-03-31 21:51:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,10 @@ class Artist
   attr_accessor :name
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+  #def initialize(name)
+  #  @name = name
+  #  @songs = []
+  #end
 
   def self.all
     @@all
